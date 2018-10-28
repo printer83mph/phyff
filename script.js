55bad0841f2c237f1aa8e4423da85fa9d0309f38
printer83mph <printer.83mph@gmail.com>
2018-10-28 19:41:39 -0400

    Polish and structure

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var img;
+
 window.onload = function() {
-  
+
+    img = document.getElementById("top");
+
+    window.addEventListener("scroll", function() {
+        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        if (scrollTop === 0) {
+            img.classList.remove("smaller");
+        } else {
+            img.classList.add("smaller");
+        }
+    });
+
 }
