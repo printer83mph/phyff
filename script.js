059cfb71d011657f0ccca3e4b1bbe35241222fd8
Thomas Shaw <printer.83mph@gmail.com>
2018-11-02 11:17:12 -0400

    Some content filler/WIP stuff

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ window.onload = function() {
 
     img = document.getElementById("top");
 
-    window.addEventListener("scroll", function() {
+    function checkScroll() {
         var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
         if (scrollTop === 0) {
             img.classList.remove("smaller");
         } else {
             img.classList.add("smaller");
         }
-    });
+    }
+
+    window.addEventListener("scroll", checkScroll);
+
+    checkScroll();
 
 }
