9b0cd4d085b60ec050c2115db91b3d9b2ae788b5
printer83mph <printer.83mph@gmail.com>
2019-01-23 22:25:31 -0500

    Make top bar shrink when scrolled down on mobile

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-var backUp;
+var backUp, topBtn;
 
 window.onload = function() {
     backUp = document.getElementById("back-up");
+    topBtn = document.getElementById("top");
     document.addEventListener("scroll", function() {
         if (window.scrollY > window.innerHeight) {
             backUp.classList.add("floating-back-up");
+            topBtn.classList.add("small");
         } else {
             backUp.classList.remove("floating-back-up");
+            if (window.scrollY > 45) {
+                topBtn.classList.add("small");
+            } else {
+                topBtn.classList.remove("small");
+            }
         }
     });
 }
