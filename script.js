5802453a6f5f4742b0f477f3e0cca09d8fd0ef37
printer83mph <printer.83mph@gmail.com>
2019-01-30 22:28:26 -0500

    Beautify and finish attend section (mostly)

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var backUp, topBtn;
 
-window.onload = function() {
+window.onload = function () {
     backUp = document.getElementById("back-up");
     topBtn = document.getElementById("top");
-    document.addEventListener("scroll", function() {
+
+    document.addEventListener("scroll", function () {
         if (window.scrollY > window.innerHeight) {
             backUp.classList.add("floating-back-up");
             topBtn.classList.add("small");
@@ -16,4 +17,15 @@ window.onload = function() {
             }
         }
     });
+
+    var els = document.getElementsByClassName("expand-btn");
+
+    for (let i = 0; i < els.length; i++) {
+        els[i].addEventListener("click", function () {
+            var text = this.nextElementSibling;
+            text.style.maxHeight = (text.style.maxHeight ? null : text.scrollHeight + 10 + "px");
+            text.classList.toggle("open");
+        });
+    }
+
 }
