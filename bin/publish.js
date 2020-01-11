380b05d0055a7b917c42b6b0bb7d47c55e1715b1
Thomas Shaw <printer.83mph@gmail.com>
2020-01-11 12:38:58 +0100

    Fix the funnyman

--- bin/publish.js
+++ bin/publish.js
@@ -1,11 +1,11 @@
 const ghPages = require('gh-pages');
 
+console.log("it is publishing");
+
 ghPages.publish('public', {
     push: true,
-    src: "**/*!.map",
+    src: "**/*!(.map)",
     message: "Build and deploy to pages"
 }, function(err) {
-    if (!err) {
-        console.log("Success!");
-    }
+    
 });
